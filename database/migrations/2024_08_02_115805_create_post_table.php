bfc098e72e33eb91a440e9b6a95f1b24637f774a
Krishkumar Patel <kpatel9@myune.edu.au>
2024-08-02 21:05:30 +0800

    added columns to post table. updated create_post_table.php file

--- database/migrations/2024_08_02_115805_create_post_table.php
+++ database/migrations/2024_08_02_115805_create_post_table.php
@@ -1,4 +1,5 @@
 <?php
+// Command to create this file: php artisan make:migration create_post_table
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('post', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('message');
             $table->timestamps();
         });
     }
