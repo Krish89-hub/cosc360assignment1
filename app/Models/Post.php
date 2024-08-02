1a376ff120d840c392f327c97fa26d7799eb05b3
Krishkumar Patel <kpatel9@myune.edu.au>
2024-08-02 20:22:10 +0800

    Define Resource routes for posts in web.php. Generated the Port model and created the Migration for the posts table

---
+++ app/Models/Post.php
@@ -0,0 +1,12 @@
+<?php
+// Command to create this file: php artisan make:model Post
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\PostsController; // to define resource routes  for posts
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+* creates multiple routes, handling CRUD (Create, Read, Update, and Delete) operations
+* for the Posts resource
+* URLS:
+* /posts               (GET)
+* /posts/create        (GET)
+* /posts               (POST)
+# /posts/{posts}       (GET)
+* /posts/{posts}/edit  (GET)
+* ...
+*/
+Route::resource('posts', PostsController::class);
